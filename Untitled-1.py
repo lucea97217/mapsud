428dda3c9e8e14f8f89c310251b252fac7569ebe
lucea97217 <57227326+lucea97217@users.noreply.github.com>
2021-11-10 16:25:55 +0100

    Update Untitled-1.py
    changement avec coordonnées gps

--- Untitled-1.py
+++ Untitled-1.py
@@ -66,7 +66,7 @@ from pyproj import Proj, transform
 inProj = Proj(init='epsg:2154')
 outProj = Proj(init='epsg:4326')
 
-df = pd.read_table("/Users/lucea/Documents/Projetgroupe6/RécupérationDesDonnées/Gares-peage_2019.csv" , sep = ';' )
+df = pd.read_table("/Users/lucea/Documents/Projetgroupe6/RécupérationDesDonnées/Gares-peage_2019.csv" , sep = ';' )
 
 # print(df.columns)
 
@@ -85,8 +85,38 @@ print(GPS)
 
 
 # %%
+import osmnx
+x1,y1= GPS[1]
+x2,y2 = GPS[10]
+osmnx.distance.euclidean_dist_vec(y1,x1,y2,x2)
 
-from pyproj import Proj, transform
+
+# %%
+import alphashape
+from descartes import PolygonPatch
+import folium
+import geopandas as gpd
+from geopy.geocoders import Nominatim
+from ipywidgets import interact, fixed, widgets
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import osmnx as ox
+import pandas as pd
+from shapely import geometry
 
 
+x1,y1= GPS[1]
+x2,y2 = GPS[10]
+
+%config InlineBackend.figure_format = 'retina'
+plt.rcParams['figure.figsize'] = (10, 10)
+address = 'Montpellier, 34000, France'
+geocoder = Nominatim(user_agent='Isochrone calculator')
+location = geocoder.geocode(address)
+location 
+
+m = folium.Map((location.latitude, location.longitude), max_zoom=20, zoom_start=16)
+folium.Marker((location.latitude, location.longitude), popup=address).add_to(m)
+m
 # %%
