f2a375b578076a7d451322144e593dde36aa85a0
lucea97217 <lucea.lenny@hotmail.com>
2021-11-29 14:43:17 +0100

    mise a jour interact

--- Données/graph.py
+++ Données/graph.py
@@ -71,41 +71,42 @@ def k_opti(i,j):
 class graphique:
     def __init__(self) -> None:
         pass
-    def graph_rang(char1,char2,k):
-        if isinstance(char1,str)==True and isinstance(char2,str)==True:
-            i = nomCoord(char1)
-            j = nomCoord(char2)
-            print(i,j)
-            if k >= k_opti(i,j):
-                k = k_opti(i,j)
-                print("Au dessus de "+ str(k)+" " +" sorties supplémentaires le prix reste inchangé.")
+    def graph_rang(DEPART,ARRIVEE,nbSorties):
+        if isinstance(DEPART,str)==True and isinstance(ARRIVEE,str)==True:
+            if DEPART == ARRIVEE:
+                return 'Trajet impossible'
             else:
-                k=k
+                j = nomCoord(ARRIVEE)
+                if nbSorties >= k_opti(i,j):
+                    nbSorties = k_opti(i,j)
+                    print("Au dessus de "+ str(nbSorties)+" " +" sorties supplémentaires le prix reste inchangé.")
+                else:
+                    nbSorties=nbSorties
             
-            li = trajet_optimal_min(i,j)[k][1]
-            locationList=[]
-            for point in range(len(li)):
-                locationList.append(latLong(li[point]))
-            coords = (longLat(i),longLat(j))
-            client = openrouteservice.Client(key='5b3ce3597851110001cf6248ec32a01981c344289c76bd7dbc72c78d')
-            res = client.directions(coords,preference="fastest")
-            geometry = client.directions(coords)['routes'][0]['geometry']
-            decoded = convert.decode_polyline(geometry)
-            distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"
-            duration_txt = "<h4> <b>Durée :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['duration']/60,1))+" Mins. </strong>" +"</h4></b>"
-            price_txt = "<h4> <b>Prix :&nbsp" + "<strong>"+ str(round((trajet_optimal_min(i,j)[k][0])/round(res['routes'][0]['summary']['distance']/1000,1),2))+" € /km. </strong>" +"</h4></b>"
-            m = folium.Map(location=[Lat(i),long(i)],zoom_start=10, control_scale=True,tiles="cartodbpositron")
-            folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+duration_txt+price_txt,max_width=300)).add_to(m)
-            for loc in range(len(locationList)):
+                li = trajet_optimal_min(i,j)[nbSorties][1]
+                locationList=[]
+                for point in range(len(li)):
+                    locationList.append(latLong(li[point]))
+                coords = (longLat(i),longLat(j))
+                client = openrouteservice.Client(key='5b3ce3597851110001cf6248ec32a01981c344289c76bd7dbc72c78d')
+                res = client.directions(coords,preference="fastest")
+                geometry = client.directions(coords)['routes'][0]['geometry']
+                decoded = convert.decode_polyline(geometry)
+                distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"
+                duration_txt = "<h4> <b>Durée :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['duration']/60,1))+" Mins. </strong>" +"</h4></b>"
+                price_txt = "<h4> <b>Prix :&nbsp" + "<strong>"+ str(round((trajet_optimal_min(i,j)[nbSorties][0])/round(res['routes'][0]['summary']['distance']/1000,1),2))+" € /km. </strong>" +"</h4></b>"
+                m = folium.Map(location=[Lat(i),long(i)],zoom_start=10, control_scale=True,tiles="cartodbpositron")
+                folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+duration_txt+price_txt,max_width=300)).add_to(m)
+                for loc in range(len(locationList)):
 
-                folium.Marker(
-                    locationList[loc],
-                    popup=df["NOMGARE"][loc+i],
-                    icon=folium.Icon(icon_color='black',icon='road')
-                ).add_to(m)
+                    folium.Marker(
+                        locationList[loc],
+                        popup=df["NOMGARE"][loc+i],
+                        icon=folium.Icon(icon_color='black',icon='road')
+                    ).add_to(m)
 
             
-            return m
+                return m
         else:
             return "Vos variables n'ont pas le bon format ou utilisez la fonction 'nomCoord'"
 
@@ -132,10 +133,9 @@ for i in range(11):
 
 #%%
 
-interact(graphique.graph_rang,char1= df_nom, char2= df_nom2 ,k = k)
-#%%
-######### histogramme
+interact(graphique.graph_rang,DEPART= df_nom, ARRIVEE= df_nom,nbSorties = k)
 
-nomCoord('VENDARGUES')
+
+
 
 # %%
