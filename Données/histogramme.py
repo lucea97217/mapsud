4f88998f8c9c11fa5f0fb16b13ea6e3f399c5071
JiayueY <91314965+JiayueY@users.noreply.github.com>
2021-11-30 12:40:47 +0100

    Update histogramme.py

--- Données/histogramme.py
+++ Données/histogramme.py
@@ -3,23 +3,21 @@ import pandas
 from algo_tarifs import *
 import plotly.express as px
 #%%
-def histogramme(A,B):
-     """fonctions qui permet d'avoir un histogramme"""
-     
-     R = trajet_optimal(A,B)
+
+R = trajet_optimal(A,B)
 
 
-     name_axe_x = "Nombre de sortie maximale autorisée"
-     name_axe_y = "prix (€)"
-     d = {name_axe_x: [], name_axe_y: []}
-     for n in range(len(R)):
-          d[name_axe_x].append(R[n][2])
-          d[name_axe_y].append(R[n][0])
+name_axe_x = "Nombre de sortie maximale autorisée"
+name_axe_y = "prix (€)"
+d = {name_axe_x: [], name_axe_y: []}
+for n in range(len(R)):
+     d[name_axe_x].append(R[n][2])
+     d[name_axe_y].append(R[n][0])
 
-     data = pandas.DataFrame(d)
+data = pandas.DataFrame(d)
 
-     fig = px.bar(data, x=name_axe_x, y=name_axe_y)
+fig = px.bar(data, x=name_axe_x, y=name_axe_y)
 
-     fig.show()
+fig.show()
 
 # %%
