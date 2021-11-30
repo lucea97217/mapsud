123628c5f31552e8cdce43de5af10f15948d03e3
JiayueY <91314965+JiayueY@users.noreply.github.com>
2021-11-30 18:27:30 +0100

    opti

---
+++ Données/graph.py
@@ -0,0 +1,165 @@
+#%%
+from numpy import string_
+import pandas as pd
+import openrouteservice
+from openrouteservice import convert
+import folium
+import json
+from algo_tarifs import *
+from ipywidgets import interact
+from histogramme import histo
+from donnees import *
+
+#%%
+#calcul du nombre optimum de sorties
+#Pour payer le tarif le moins élevé d'un péage à un autre.
+def k_opti(S):
+#si la fonction retourne -1 c'est qu'il n'y a pas 
+#d'itinéraire possible
+    if S ==-1:
+        return S
+
+    return len(S)-1
+##############################################################
+#%%
+######################## GRAPHE INTERACTIF ###################
+
+# Creation classe graphique
+# Elle permet d'afficher un graphe interactif 
+class graphique:
+    def __init__(self) -> None:
+        pass
+
+    def graph_rang(DEPART,ARRIVEE,nbSorties):
+    # Ici on devra insérer 2 chaines de caractères
+    # Correspondant à des noms de gares connus
+    # Avec une contrainte de nombre de sorties (nbSorties)
+
+        
+        if isinstance(DEPART,str)==True and isinstance(ARRIVEE,str)==True:
+        #Verification des variables
+            if DEPART == ARRIVEE:
+            # Evidemment dans ce cas il n'y a pas de trajet
+                return 'Trajet impossible'
+            else:
+                # On utilise nomCoord présentée ultérieurement
+                i = nomCoord(DEPART)
+                j = nomCoord(ARRIVEE)
+
+                S = trajet_optimal_min(i,j)
+                # On traite le cas où il n'y a pas d'itinéraire possible
+                if k_opti(S)==-1:
+                    # On affiche alors une carte vide
+                    m = folium.Map(location=[Lat(i),long(i)],zoom_start=10, control_scale=True,tiles="cartodbpositron")
+                    print("Pas d'itinéraire possible")
+                    return m 
+                
+                # Si l'utilisateur insère un contrainte supérieure
+                # Au nombre de sortie optimum 
+                # On réinitialise alors la contrainte
+                elif nbSorties >= k_opti(S):
+                    nbSorties = k_opti(S)
+        
+                else:
+                
+                # Sinon la contrainte reste inchangée
+
+                    nbSorties=nbSorties
+
+                # On spécifie tout de même l'itinéraire optimal
+                print("Itinéraire optimal de "+ str(k_opti(S)) +" "+"sorties.")
+
+                # On liste les sorties à l'aide des numéros arrangés
+                li = trajet_optimal_min(i,j)[nbSorties][1]
+                locationList=[]
+                # Dans locationList nous insérons les coordonnées 
+                # Correspondant numéros arrangés de li
+                for point in range(len(li)):
+                    locationList.append(latLong(li[point]))
+                
+                # Création de l'itinéraire entre 
+                # Le point de départ et le point d'arrivée
+                coords = (longLat(i),longLat(j))
+
+                # Utilisation de openrouteservice
+                # Il faudra ici utiliser une clef API
+                client = openrouteservice.Client(key='5b3ce3597851110001cf6248ec32a01981c344289c76bd7dbc72c78d')
+
+                # Ici on calculera l'itinéraire entre nos deux points
+                # En utilisant preference="fastest" 
+                # Afin d'être sur de rester sur l'autoroute 
+                res = client.directions(coords,preference="fastest")
+                geometry = client.directions(coords)['routes'][0]['geometry']
+                decoded = convert.decode_polyline(geometry)
+
+                # Fonction pour rendre l'itinéraire cliquable
+                # On insère les information de distance 
+                # De durée et de coût du trajet
+                distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"
+                duration_txt = "<h4> <b>Durée :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['duration']/60,1))+" Mins. </strong>" +"</h4></b>"
+                price_txt = "<h4> <b>Prix :&nbsp" + "<strong>"+ str(round((trajet_optimal_min(i,j)[nbSorties][0])/round(res['routes'][0]['summary']['distance']/1000,1),2))+" € /km. Soit </strong>"+str((trajet_optimal_min(i,j)[nbSorties][0]))+" €  </strong>" +"</h4></b>"
+                
+                # Initialisation carte
+                m = folium.Map(location=[Lat(i),long(i)],zoom_start=10, control_scale=True,tiles="cartodbpositron")
+                folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+duration_txt+price_txt,max_width=300)).add_to(m)
+
+                # Initialision des points correspondants aux péages
+                for loc in range(len(locationList)):
+
+                    folium.Marker(
+                        locationList[loc],
+                        popup=df["NOMGARE"][li[loc]],
+                        icon=folium.Icon(icon_color='black',icon='road')
+                    ).add_to(m)
+
+                histo(DEPART,ARRIVEE)
+                return m
+        else:
+            return "Vos variables n'ont pas le bon format ou utilisez la fonction 'nomCoord'"
+
+    # Fonction calcul distance
+    def distance(i,j):
+        coords = ((df["X"][i],df["Y"][i]),(df["X"][j],df["Y"][j]))
+        client = openrouteservice.Client(key='5b3ce3597851110001cf6248ec32a01981c344289c76bd7dbc72c78d')
+        res = client.directions(coords, preference="fastest")
+        dist=float(round(res['routes'][0]['summary']['distance']/1000,1))
+        return dist
+
+##############################################################################
+#%%
+
+############### INITIALISATION CARTE AVEC WIDGETS #########################
+
+# Initialisation d'une liste d'entiers
+# Allant de 1 à 10
+# Pour l'initialisation de la contrainte de sorties
+k=[]
+for i in range(11):
+    k.append(i)
+#%%
+# Ici on a repris Y de algo_tarifs
+# Correspondant à la liste des numéros arrangés
+# Des péages dont nous pouvons entrer et sortir
+
+Y = [ 0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 27, 29, 30, 31, 33, 35, 36, 37, 38, 39, 40, 41, 42 ]
+df_nom=[]
+
+# On insère alors dans df_nom tout les noms de gare
+# Correspondants aux numéros arrangés de Y
+for i in range(len(Y)):
+    df_nom.append(df["NOMGARE"][Y[i]])
+
+#%%
+
+# On obtient alors enfin un graphe interactif
+# Ou on peut sélectionner notre lieux de depart
+# Notre lieux d'arrivée 
+# Et notre contrainte de nombre de sorties supplémentaires
+interact(graphique.graph_rang,DEPART= df_nom, ARRIVEE= df_nom,nbSorties = k)
+
+############################## FIN ##########################################
+# %%
+
+# %%
+
+# %%
--- Données/histogramme.py
+++ Données/histogramme.py
@@ -1,19 +1,48 @@
+#%%
 import pandas
-from algo_tarifs import *
+from algo_tarifs import trajet_optimal_min
 import plotly.express as px
+from ipywidgets import interact
+import matplotlib.pyplot as plt
+import pandas as pd
+from donnees import *
+#%%
 
-R = trajet_optimal(A,B)
+def histo(DEPART,ARRIVEE):
+     if DEPART == ARRIVEE:
+          name_axe_x = "Nombre de sortie maximale autorisée"
+          name_axe_y = "prix (€)"
+          d = {name_axe_x: [], name_axe_y: []}
+          data = pandas.DataFrame(d)
+          plt.bar(d[name_axe_x],d[name_axe_y])
+          print(" Pas d'itinéraire possible.")
+          return plt.show()
 
 
-name_axe_x = "Nombre de sortie maximale autorisée"
-name_axe_y = "prix (€)"
-d = {name_axe_x: [], name_axe_y: []}
-for n in range(len(R)):
-     d[name_axe_x].append(R[n][2])
-     d[name_axe_y].append(R[n][0])
+     R = trajet_optimal_min(nomCoord(DEPART),nomCoord(ARRIVEE))
 
-data = pandas.DataFrame(d)
+     name_axe_x = "Nombre de sortie maximale autorisée"
+     name_axe_y = "prix (€)"
+     d = {name_axe_x: [], name_axe_y: []}
+     for n in range(len(R)):
+          d[name_axe_x].append(R[n][2])
+          d[name_axe_y].append(R[n][0])
 
-fig = px.bar(data, x=name_axe_x, y=name_axe_y)
+     data = pandas.DataFrame(d)
 
-fig.show()
+     if d[name_axe_y]==[0] and d[name_axe_x]==[0]:
+          plt.scatter(d[name_axe_x],d[name_axe_y])
+          return plt.show()
+
+     plt.bar(d[name_axe_x],d[name_axe_y])
+
+     return plt.show()
+
+     
+
+
+# %%
+interact(histo,DEPART = df_nom, ARRIVEE = df_nom)
+
+
+# %%
