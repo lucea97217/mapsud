3872433edf95bd8ac6b26b31f845a385c3411b3e
lucea97217 <lucea.lenny@hotmail.com>
2021-12-07 21:37:35 +0100

    mapsud

--- Données/test.py
+++ Données/test.py
@@ -1,23 +1,14 @@
 #%%
-import unittest
+import pytest
 
 from mapsud.graph import trajet_optimal,trajet_optimal_min
 #%%
 #On test les cas particuliers
-class TestTraj(unittest.TestCase):
-    def test_traj1(self):
-        result=trajet_optimal_min(41,42)
-        self.assertEqual(result,-1)
 
-    def test_traj1(self):
-        result=trajet_optimal(41,42)
-        self.assertEqual(result,-1)
-        
-    def test_taj3(self):
-        result=trajet_optimal(1,3)
-        self.assertEqual(result,[(0, [1, 3], 0), (0, [1, 3], 1)])
-
-if __name__=='__main__':
-    unittest.main()
+def test_traj1():
+    assert trajet_optimal_min(41,42) == -1
+   
+def test_traj1():
+    assert trajet_optimal(41,42) ==-1
 
 # %%
