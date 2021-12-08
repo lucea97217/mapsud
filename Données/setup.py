3d6554f69e8c62c9cd4850abeea9e48287f90a77
lucea97217 <lucea.lenny@hotmail.com>
2021-12-08 18:20:43 +0100

    mapsud

--- Données/setup.py
+++ Données/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 import mapsud
 
 setup(name = 'mapsud',
-      version = '0.0.2',
+      version = '0.0.3',
       url='https://github.com/lucea97217/mapsud.git',
       author = 'LUCEA, VALDEYRON, YUAN',
       author_email = 'lenny.lucea@etu.umontpellier.fr',
       maintainer = 'LUCEA, VALDEYRON, YUAN',
       maintainer_email = 'lenny.lucea@etu.umontpellier.fr',
       keywords = 'Tarifs autoroutiers sud France',
-      packages = ['mapsud'],
+      packages = ['mapsud','test'],
       description = 'voir presentation',
       license = 'MIT',
       platforms = 'ALL',
