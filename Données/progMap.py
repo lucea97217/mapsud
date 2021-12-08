5dd684ece6ec4feee845458eb26882b1610b3c9b
lucea97217 <lucea.lenny@hotmail.com>
2021-12-08 18:12:10 +0100

    mapsud

--- Données/progMap.py
+++ Données/progMap.py
@@ -3,12 +3,10 @@
 #%%
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-from mapsud.graph import *
+from mapsud.graph import map
 
 #%%
 
+map('your Key')
+
 ################################################################################
-
-
-
-# %%
