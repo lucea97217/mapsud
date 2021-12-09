69d9ff7bf7914c88b111efe3a9e7e82a9f70b74a
Lenny Lucea <57227326+lucea97217@users.noreply.github.com>
2021-12-09 18:05:24 +0100

    Update test_mapsud.py

--- Données/test/test_mapsud.py
+++ Données/test/test_mapsud.py
@@ -3,17 +3,16 @@ import sys
 import os
 import pytest
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
-#import mapsud
-#from mapsud.graph import trajet_optimal_min, trajet_optimal
+import mapsud
+from mapsud.graph import trajet_optimal_min, trajet_optimal
 
 #%%
 #On test les cas particuliers
-def calc():
-    assert 2 == 2
-#def test_traj1():
-#    assert trajet_optimal_min(41,42) == -1 
+
+def test_traj1():
+    assert trajet_optimal_min(41,42) == -1 
 
    
-#def test_traj2():
-#    assert trajet_optimal(41,42) == -1
+def test_traj2():
+    assert trajet_optimal(41,42) == -1
 
